0e157fe4bd4d1aaae61e70f150ebe7974807cda6
srushtimane27 <srushtimane31@gmail.com>
2022-10-13 14:56:16 +0530

    Game start with single player at start position 0

---
+++ src/com/Bridgelabz/SnakeAndLadder/SnakeAndLadder.java
@@ -0,0 +1,10 @@
+package com.Bridgelabz.SnakeAndLadder;
+
+public class SnakeAndLadder {
+    public static int PLAYER_POSITION = 0;
+
+
+    public static void main(String[] args) {
+        System.out.println("Start Position : " + PLAYER_POSITION);
+    }
+}
